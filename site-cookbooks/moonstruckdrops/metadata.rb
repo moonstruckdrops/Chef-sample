b03788667aa9976e400353c55b385f1654c92fc2
moonstruckdrops <moonstruckdrops@gmail.com>
2014-03-09 21:07:11 +0900

    設定誤りを修正

--- site-cookbooks/moonstruckdrops/metadata.rb
+++ site-cookbooks/moonstruckdrops/metadata.rb
@@ -6,5 +6,5 @@ description      'Installs/Configures moonstruckdrops'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends "yum","~>3.1.2"
+depends "yum"
 depends "yum-epel"
