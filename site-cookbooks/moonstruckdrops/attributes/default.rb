dd22b7840b24654b490c953d92aa982fa9ba2d9c
moonstruckdrops <moonstruckdrops@gmail.com>
2014-03-09 21:04:32 +0900

    XWindowのインストールレシピ設定の追加

--- site-cookbooks/moonstruckdrops/attributes/default.rb
+++ site-cookbooks/moonstruckdrops/attributes/default.rb
@@ -1,8 +1,12 @@
 # package
 default["moonstruckdrops"]["yum"]["base"]["packages"] = ["emacs", "wget", "make", "gcc", "rpm-build", "vnc-server", "git", "subversion", "samba"]
+default["moonstruckdrops"]["yum"]["epel"]["install"] = false
 default["moonstruckdrops"]["yum"]["epel"]["packages"] = ["bash_completion", "ngircd", "ntfs-3g"]
+default["moonstruckdrops"]["yum"]["rpmforge"]["install"] = false
 default["moonstruckdrops"]["yum"]["rpmforge"]["packages"] = ["tmux"]
+default["moonstruckdrops"]["yum"]["remi"]["install"] = false
 default["moonstruckdrops"]["yum"]["remi"]["packages"] = ["firefox"]
+
 # jenkins
 default["moonstruckdrops"]["yum"]["jenkins"]["install"] = false
 default["moonstruckdrops"]["yum"]["jenkins"]["packages"] = ["jenkins"]
@@ -12,6 +16,7 @@ default["moonstruckdrops"]["yum"]["jenkins"]["baseurl"] = "http://pkg.jenkins-ci
 default["moonstruckdrops"]["yum"]["jenkins"]["gpgkey"] = "http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key"
 default["moonstruckdrops"]["yum"]["jenkins"]["gpgcheck"] = true
 default["moonstruckdrops"]["yum"]["jenkins"]["enabled"] = false
+
 # KVM
 # http://docs.fedoraproject.org/en-US/Fedora/14/html/Software_Management_Guide/ch05s15.html
 default["moonstruckdrops"]["virtual"]["install"] = false
@@ -19,5 +24,8 @@ default["moonstruckdrops"]["virtual"]["group"]["packages"] = ["@Virtualization",
 
 # X Window
 default["moonstruckdrops"]["xwindow"]["install"] = false
-default["moonstruckdrops"]["xwindow"]["group"]["packages"] = ["X Window System", "GNOME Desktop Environment", "Desktop", "Desktop Platform"]
+default["moonstruckdrops"]["xwindow"]["group"]["packages"] = ["@X Window System", "@GNOME Desktop Environment", "@Desktop", "@Desktop Platform"]
 default["moonstruckdrops"]["xwindow"]["font"]["packages"] = ["xorg-x11-font*"]
+default["moonstruckdrops"]["xwindow"]["japanese"]["support"] = true
+default["moonstruckdrops"]["xwindow"]["japanese"]["package"] = "@Japanese Support"
+default["moonstruckdrops"]["xwindow"]["runlevel"] = 3
--- site-cookbooks/moonstruckdrops/recipes/development.rb
+++ site-cookbooks/moonstruckdrops/recipes/development.rb
@@ -31,14 +31,65 @@ if node["moonstruckdrops"]["yum"]["jenkins"]["install"]
    end
 end
 
-#include_recipe "yum-epel"
-#if node["moonstruckdrops"]["virtual"]["managed"]
-#   case node[:platform]
-#   when "centos"
-# 	  node["moonstruckdrops"]["virtual"]["group"]["packages"].each do |package|
-# 		 yum_package package do
-# 			action :install
-# 		 end
-# 	  end
-#   end
-#end
+# X Window
+if node["moonstruckdrops"]["xwindow"]["install"]
+   case node[:platform]
+   when "centos"
+      node["moonstruckdrops"]["xwindow"]["group"]["packages"].each do |package|
+         yum_package package do
+            flush_cache [:before]
+            action :install
+         end
+      end
+
+      yum_package node["moonstruckdrops"]["xwindow"]["font"]["packages"] do
+         action :install
+      end
+
+      if node["moonstruckdrops"]["xwindow"]["japanese"]["support"]
+         yum_package node["moonstruckdrops"]["xwindow"]["japanese"]["package"] do
+            flush_cache [:before]
+            action :install
+         end
+      end
+
+      template "/etc/inittab" do
+         source "inittab.erb"
+         owner "root"
+         group "root"
+         mode "0644"
+         variables({
+               :runlevel => node["moonstruckdrops"]["xwindow"]["runlevel"]
+            })
+      end
+   end
+end
+
+# KVM
+if node["moonstruckdrops"]["virtual"]["install"]
+   case node[:platform]
+   when "centos"
+      node["moonstruckdrops"]["virtual"]["group"]["packages"].each do |package|
+         yum_package package do
+            flush_cache [:before]
+            action :install
+         end
+      end
+   end
+end
+
+# epel package
+if node["moonstruckdrops"]["yum"]["epel"]["install"]
+   include_recipe "yum-epel"
+
+   node["moonstruckdrops"]["yum"]["epel"]["packages"].each do |package|
+      yum_package package do
+         options "--enablerepo=epel"
+         flush_cache [:before]
+         action :install
+      end
+   end
+end
+
+# iptables
+
